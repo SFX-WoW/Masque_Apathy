bd0c45ff2f15df7cfb5c908b85725209e99a7d09
StormFX <stormfx@gmail.com>
2018-11-01 12:46:10 -0500

    - Add Locales.lua.

---
+++ Locales.lua
@@ -0,0 +1,43 @@
+--[[
+
+	This file is part of 'Masque: Apathy', an add-on for World of Warcraft. For license information,
+	please see the included License.txt file.
+
+	* File....: Locales.lua
+	* Authors.: StormFX
+
+]]
+
+-- GLOBALS: GetLocale
+
+local _, Core = ...
+
+----------------------------------------
+-- Locales
+----------------------------------------
+
+local L = {}
+
+Core.Locale = setmetatable(L, {
+	__index = function(self, k)
+		self[k] = k
+		return k
+	end
+})
+
+local Locale = GetLocale()
+
+if Locale == "enGB" or Locale == "enUS" then
+	--L["A minimalistic skin with thin borders and a shadow."] = "A minimalistic skin with thin borders and a shadow."
+	--L["An alternate version of Apathy without a shadow."] = "An alternate version of Apathy without a shadow."
+	return
+--elseif Locale == "deDE" then
+--elseif Locale == "esES" or Locale == "esMX" then
+--elseif Locale == "frFR" then
+--elseif Locale == "itIT" then
+--elseif Locale == "koKR" then
+--elseif Locale == "ptBR" then
+--elseif Locale == "ruRU" then
+--elseif Locale == "zhCN" then
+--elseif Locale == "zhTW" then
+end
