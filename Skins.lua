0cf677b4021de70d3b938ab7d0454545b5e3c745
StormFX <stormfx@gmail.com>
2018-11-01 12:46:28 -0500

    - Update skin info.

--- Skins.lua
+++ Skins.lua
@@ -3,23 +3,49 @@
 	This file is part of 'Masque: Apathy', an add-on for World of Warcraft. For license information,
 	please see the included License.txt file.
 
-	* File...: Apathy.lua
+	* File...: Skins.lua
 	* Author.: StormFX
 
+	Apathy Skins
 ]]
 
-local MSQ = LibStub("Masque", true)
+-- GLOBALS: GetAddOnMetadata, LibStub
+
+local MSQ = LibStub and LibStub("Masque", true)
 if not MSQ then return end
 
-local AddOn, _ = ...
-local Version = GetAddOnMetadata(AddOn, "Version")
+----------------------------------------
+-- Locals
+----------------------------------------
 
+local AddOn, Core = ...
+
+-- Locale
+local L = Core.Locale
+
+-- Skin Info
+local Version = GetAddOnMetadata(AddOn, "Version")
+local Authors = "StormFX"
+local Websites = {
+	"https://github.com/stormfx/masque_apathy",
+	"https://www.wowace.com/projects/masque-apathy",
+	"https://www.curseforge.com/wow/addons/masque-apathy",
+	"https://www.wowinterface.com/downloads/info8870",
+}
+
+----------------------------------------
 -- Apathy
+----------------------------------------
+
 MSQ:AddSkin("Apathy", {
-	Author = "StormFX",
-	Version = Version,
 	Shape = "Square",
-	Masque_Version = 70200,
+	Group = "Apathy",
+	Order = 1,
+	Version = Version,
+	Authors = Authors,
+	Websites = Websites,
+	Description = L["A minimalistic skin with thin borders and a shadow."],
+	Masque_Version = 80000,
 	Backdrop = {
 		Width = 32,
 		Height = 32,
@@ -34,15 +60,6 @@ MSQ:AddSkin("Apathy", {
 		Height = 32,
 		Color = {1, 0, 0, 0.3},
 		Texture = [[Interface\AddOns\Masque_Apathy\Textures\Overlay]],
-	},
-	Cooldown = {
-		Width = 28,
-		Height = 28,
-		Color = {0, 0, 0, 0.7},
-	},
-	ChargeCooldown = {
-		Width = 28,
-		Height = 28,
 	},
 	Pushed = {
 		Width = 32,
@@ -114,6 +131,15 @@ MSQ:AddSkin("Apathy", {
 		Height = 10,
 		OffsetY = -2,
 	},
+	Cooldown = {
+		Width = 28,
+		Height = 28,
+		Color = {0, 0, 0, 0.7},
+	},
+	ChargeCooldown = {
+		Width = 28,
+		Height = 28,
+	},
 	Shine = {
 		Width = 26,
 		Height = 26,
@@ -122,9 +148,15 @@ MSQ:AddSkin("Apathy", {
 	},
 }, true)
 
+----------------------------------------
 -- Apathy - No Shadow
+----------------------------------------
+
 MSQ:AddSkin("Apathy - No Shadow", {
+	Title = "No Shadow",
+	Order = 2,
 	Template = "Apathy",
+	Description = L["An alternate version of Apathy without a shadow."],
 	Normal = {
 		Width = 32,
 		Height = 32,
