c57fd48e291d9924e651971f6e39ba2151d4d3f5
StormFX <stormfx@gmail.com>
2024-06-24 23:12:14 -0500

    - Add custom animation support. (Closes #54)

--- Skins.lua
+++ Skins.lua
@@ -25,7 +25,7 @@ local L = Core.Locale
 -- Locals
 ---
 
-local API_VERSION = 100105
+local API_VERSION = 100207
 
 -- Skin Info
 local Version = "@project-version@"
@@ -471,7 +471,7 @@ MSQ:AddSkin("Apathy", {
 		-- SetAllPoints = nil,
 	},
 	SpellAlert = {
-		Width = 30,
-		Height = 30,
+		Width = 24,
+		Height = 24,
 	},
 })
